d760086db6e3b10015f8eca31e58b8aa2dd33e6d
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2022-10-12 11:36:16 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ for name in files:
 
 #Don't be a thief 
 print("Successfully deployed!")
-print("By MaheshChauhan • DroneBots")
+print("By Anurag Maheshwari • Am_RoBots")
 
 if __name__ == "__main__":
     bot.run_until_disconnected()
