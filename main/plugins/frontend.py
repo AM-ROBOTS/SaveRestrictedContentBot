064f0fb3a8dbbf6a8353d9d20f4e8cc33e47b695
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2022-10-12 10:47:45 +0530

    Add files via upload

---
+++ main/plugins/frontend.py
@@ -0,0 +1,46 @@
+#Github.com/8769Anurag
+
+import time, os
+
+from .. import bot as Drone
+from .. import userbot, Bot
+from .. import FORCESUB as fs
+from main.plugins.pyroplug import get_msg
+from main.plugins.helpers import get_link, join, screenshot
+
+from telethon import events
+
+from ethon.telefunc import force_sub
+
+ft = f"To use this bot you've to join @{fs}."
+
+message = "Send me the message link you want to start saving from, as a reply to this message."
+          
+# To-Do:
+# Make these codes shorter and clean
+# ofc will never do it. 
+
+@Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+async def clone(event):
+    if event.is_reply:
+        reply = await event.get_reply_message()
+        if reply.text == message:
+            return
+    try:
+        link = get_link(event.text)
+        if not link:
+            return
+    except TypeError:
+        return
+    s, r = await force_sub(event.client, fs, event.sender_id, ft)
+    if s == True:
+        await event.reply(r)
+        return
+    edit = await event.reply("Processing!")
+    if 't.me/+' in link:
+        q = await join(userbot, link)
+        await edit.edit(q)
+        return 
+    if 't.me/' in link:
+        await get_msg(userbot, Bot, event.sender_id, edit.id, link, 0)
+        
