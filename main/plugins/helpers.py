c4f30cfa67c1a30d0d01e2f40e6be99966c05bc9
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2022-10-12 11:38:05 +0530

    Update helpers.py

--- main/plugins/helpers.py
+++ main/plugins/helpers.py
@@ -1,4 +1,4 @@
-#Github.com/Vasusen-code
+#Github.com/8769Anurag
 
 from pyrogram.errors import FloodWait, InviteHashInvalid, InviteHashExpired, UserAlreadyParticipant
 from telethon import errors, events
